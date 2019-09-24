afa10ad3976e56b8233325ad8513284238e2769f
Thiago Luz <thiagotls58@gmail.com>
2019-09-23 23:13:55 -0300

    Criação da classe modelo e da base de dados
    Classe Modelo: Livro
    Classe de Contexto: LivrosContext

---
+++ Introducao/Migrations/20190924021011_Inicial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Introducao.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Livro",
+                columns: table => new
+                {
+                    LivroId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Titulo = table.Column<string>(type: "varchar(100)", nullable: true),
+                    Autor = table.Column<string>(nullable: true),
+                    AnoPublicacao = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Livro", x => x.LivroId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Livro");
+        }
+    }
+}
--- Introducao/Program.cs
+++ Introducao/Program.cs
@@ -1,12 +1,61 @@
 ﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
-namespace Introducao
+namespace EFCoreDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using(var db = new LivrosContext())
+            {
+                db.Livros.Add(new Livro 
+                    { Titulo = "Domain-Driven Design: Tackling Complexity in the Heart of Software", 
+                        Autor = "Eric Evans", AnoPublicacao = 2003 });
+                db.Livros.Add(new Livro 
+                    { Titulo = "Agile Principles, Patterns, and Practices in C#",
+                        Autor = "Robert C. Martin", AnoPublicacao = 2006 });
+
+                db.SaveChanges();
+                
+                var livros = db.Livros.ToList();
+                Console.WriteLine("--------------------RESULTADOS--------------------");
+                //db.Livros.ForEachAsync(x => Console.WriteLine($"Título: {x.Titulo} | Autor: {x.Autor}"));
+                foreach(var l in livros)
+                     Console.WriteLine($"Título: {l.Titulo} | Autor: {l.Autor}");
+            }
         }
     }
+
+    public class LivrosContext : DbContext
+    {
+        public DbSet<Livro> Livros { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EFCore.Demo;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Livro>()
+                .ToTable("Livro");
+            
+            modelBuilder.Entity<Livro>()
+                .HasKey(p => p.LivroId);
+
+            modelBuilder.Entity<Livro>()
+                .Property(p => p.Titulo)
+                .HasColumnType("varchar(100)");
+        }
+    }
+
+    public class Livro
+        {
+            public int LivroId { get; set; }
+            public string Titulo { get; set; }
+            public string Autor { get; set; }
+            public int AnoPublicacao { get; set; }
+        }
 }
